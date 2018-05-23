725d7aaff8e94ff56f321fd64569f2a2a1fba9eb
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-23 07:54:35 +0200

    Test of authors is skipped

--- DrdPlus/Tests/tests_config.php
+++ DrdPlus/Tests/tests_config.php
@@ -1,7 +1,8 @@
 <?php
-const FREE_ACCESS = true; // covered by JUST_TEXT_TESTING
-// const JUST_TEXT_TESTING = true;
+const FREE_ACCESS = true;
+const JUST_TEXT_TESTING = true;
 // const NO_EXTERNAL_ANCHORS_WITH_HASH_EXPECTED = true;
 // const NOT_FOR_PLAYERS = true;
 // SINGLE_VERSION_ONLY = true;
 // WITHOUT_SPECIFIC_JOURNAL = true;
+const NO_AUTHORS = true;
