bf07c6d2f07b00a6bc45ea61e8609f8fae4081b4
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-16 09:06:28 +0200

    Necessary update reflecting skeleton changes

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
 $documentRoot = $documentRoot ?? (PHP_SAPI !== 'cli' ? \rtrim(\dirname($_SERVER['SCRIPT_FILENAME']), '\/') : \getcwd());
-$webRoot = $webRoot ?? $documentRoot . '/web/passed';
-$vendorRoot = $vendorRoot ?? $documentRoot . '/vendor';
 
 /** @noinspection PhpIncludeInspection */
-require_once $vendorRoot . '/autoload.php';
+require_once $documentRoot . '/vendor/autoload.php';
 
 $controller = $controller ?? new \DrdPlus\RulesSkeleton\RulesController(
         'UA-121206931-3',
         \DrdPlus\RulesSkeleton\HtmlHelper::createFromGlobals($documentRoot),
-        $documentRoot,
-        $webRoot,
-        $vendorRoot
+        new \DrdPlus\RulesSkeleton\Dirs($documentRoot)
     );
 $controller->setFreeAccess();
 
