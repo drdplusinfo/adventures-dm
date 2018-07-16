682dd20bc6d66f8e802a596199aba6d633e9f798
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-16 18:38:58 +0200

    Fixed free access

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
 $documentRoot = $documentRoot ?? (PHP_SAPI !== 'cli' ? \rtrim(\dirname($_SERVER['SCRIPT_FILENAME']), '\/') : \getcwd());
-
-/** @noinspection PhpIncludeInspection */
-require_once $documentRoot . '/vendor/drd-plus/rules-skeleton/parts/rules-skeleton/safe_autoload.php';
-
-$controller = $controller ?? new \DrdPlus\RulesSkeleton\RulesController(
-        'UA-121206931-3',
-        \DrdPlus\RulesSkeleton\HtmlHelper::createFromGlobals($documentRoot),
-        new \DrdPlus\RulesSkeleton\Dirs($documentRoot)
-    );
-$controller->setFreeAccess();
+$googleAnalyticsId = 'UA-121206931-3';
+$hasFreeAccess = true;
 
 require __DIR__ . '/vendor/drd-plus/rules-skeleton/index.php';
