b3eace3e0327f4b2f993e780bae4e8b4786451b0
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-23 17:33:10 +0200

    Libraries updated

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -13,7 +13,7 @@ return array(
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
     'Gt\\Dom\\' => array($vendorDir . '/phpgt/dom/src'),
     'Granam\\' => array($vendorDir . '/granam/strict-object/Granam', $vendorDir . '/granam/scalar/Granam', $vendorDir . '/granam/string/Granam', $vendorDir . '/granam/exceptions-hierarchy/Granam', $vendorDir . '/granam/tools/Granam'),
-    'DrdPlus\\' => array($baseDir . '/DrdPlus', $vendorDir . '/drd-plus/rules-skeleton/DrdPlus', $vendorDir . '/drd-plus/frontend-skeleton/DrdPlus'),
+    'DrdPlus\\' => array($baseDir . '/DrdPlus', $vendorDir . '/drd-plus/frontend-skeleton/DrdPlus', $vendorDir . '/drd-plus/rules-skeleton/DrdPlus'),
     'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
     'DeviceDetector\\' => array($vendorDir . '/piwik/device-detector'),
     'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
